7690360b8d0f5ba190ee04a4afa0ebd58e113f0e
Yuriy6735 <42290188+Yuriy6735@users.noreply.github.com>
2019-04-20 12:33:33 +0300

    tf

--- main.tf
+++ main.tf
@@ -5,14 +5,15 @@ provider "google" {
   project = "project1-232817"
 }
 
-resource "google_storage_bucket" "bucket" {
-  name = "superdemo3"
-  location = "EU"
-}
+//resource "google_storage_bucket" "bucket" {
+//  name = "superdemo3"
+//  location = "EU"
+//
+//}
 
 resource "google_storage_bucket_object" "app" {
   name = "app.zip"
-  bucket = "superdemo3"
+  bucket = "gs://superdemo3"
   source = "./app.zip"
 
 }
@@ -21,7 +22,7 @@ resource "google_cloudfunctions_function" "get-data" {
   name                  = "get-data"
   description           = "My weather"
   available_memory_mb   = 256
-  source_archive_bucket = "${google_storage_bucket.bucket.name}"
+  source_archive_bucket = "$gs://superdemo3"
   source_archive_object = "${google_storage_bucket_object.app.name}"
   trigger_http          = true
   timeout               = 60
