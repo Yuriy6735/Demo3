6e42e868a4e10df9e267d26f4bee617082432e9f
Yuriy <yuriy.liskevich@gmail.com>
2019-04-25 20:39:40 +0300

    add tests

--- functions/getPredictions/test.py
+++ functions/getPredictions/test.py
@@ -5,7 +5,7 @@ from getPredictions.main import get_predictions
 class MyFunTest(unittest.TestCase):
 
     def test_negative(self):
-        self.assertEquals(get_predictions("") )
+        self.assertEquals(get_predictions(""), 0)
 
 
 if __name__ == '__main__':
